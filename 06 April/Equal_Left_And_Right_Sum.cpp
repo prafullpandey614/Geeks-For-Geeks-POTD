1ceaad34afb3c9db114249e53a19b2fc95bb4710
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-04-06 11:44:17 +0530

    Create Equal_Left_And_Right_Sum.cpp

---
+++ 06 April/Equal_Left_And_Right_Sum.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Array
+{
+public:
+    template <class T>
+    static void input(vector<T> &A,int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d ",&A[i]);
+        }
+    }
+ 
+    template <class T>
+    static void print(vector<T> &A)
+    {
+        for (int i = 0; i < A.size(); i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int equalSum(int N, vector<int> &A) {
+        // code here
+        int sum =0 ; 
+        int sum2 = 0 ;
+        sum = accumulate(A.begin(),A.end(),0);
+        for(int i =0 ;i < A.size(); ++i){
+            int k = sum-A[i]-sum2;
+            if(k==sum2) return i+1;
+            sum2+=A[i];
+           
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    scanf("%d ",&t);
+    while(t--){
+        
+        int N; 
+        scanf("%d",&N);
+        
+        
+        vector<int> A(N);
+        Array::input(A,N);
+        
+        Solution obj;
+        int res = obj.equalSum(N, A);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
