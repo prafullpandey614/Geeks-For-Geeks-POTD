c24b520ed3b8ff4e6ff1038596f1a3d1c840d9fd
Prafull Pandey <81959129+prafullpandey614@users.noreply.github.com>
2023-06-21 23:49:03 +0530

    Create reverse_coding.cpp

---
+++ reverse_coding.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  const int mod=  1e9+7 ;
+    int sumOfNaturals(int n) {
+              // code here
+        long sum =((long)n*(n+1))/2;
+        long long num=pow(10,9)+7;
+        return sum % num;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
